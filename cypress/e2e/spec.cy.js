18588c01dcc476040a93b522715755aa51f633d2
AprilMD <95350189+AprilMD@users.noreply.github.com>
2022-07-28 12:17:10 +0100

    add first tests

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -4,3 +4,19 @@ describe('empty spec', () => {
   })
 })
 
+describe('Index Page Test', () => {
+  it("Loads successfully", () => {
+    cy.visit('/') // fails if not running server
+  })
+
+  it('Takes us to html page', () => {
+    cy.visit('/')
+    cy.contains('HTML').click() // click on the part element that contains HTML
+    cy.url().should('include', '/html') // new page URL should include '/html'
+  })
+  it('Takes us to nunjucks page', () => {
+    cy.visit('/')
+    cy.contains('NUNJUCK').click()
+    cy.url().should('include', '/nunjuck')
+  })
+})
