77bb0791f389854a73076ac3e556244a54f07fa9
AprilMD <95350189+AprilMD@users.noreply.github.com>
2022-08-01 17:06:49 +0100

    add first page with visual styling

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -4,24 +4,33 @@ describe('empty spec', () => {
   })
 })
 
-describe('Index Page Test', () => {
+// describe('Index Page Test', () => {
+//   it('Takes us to html page', () => {
+//     cy.visit('/')
+//     cy.contains('HTML').click() // click on the part element that contains HTML
+//     cy.url().should('include', '/html') // new page URL should include '/html'
+//   })
+//   it('Takes us to nunjucks page', () => {
+//     cy.visit('/')
+//     cy.contains('NUNJUCK').click()
+//     cy.url().should('include', '/nunjuck')
+//   })
+//   it('Takes us to spacecats page', () => {
+//     cy.visit('/')
+//     cy.contains('SPACECATS').click()
+//     cy.url().should('include', '/spacecats')
+//   })
+// })
+
+describe('Start Page Test', () => {
   it("Loads successfully", () => {
     cy.visit('/') // fails if not running server
   })
+  it("Has heading with 'confirm your identity'", () => {
+    cy.visit('/')
+    cy.get('title').should('contain', 'Confirm your identity')
+  })
 
-  it('Takes us to html page', () => {
-    cy.visit('/')
-    cy.contains('HTML').click() // click on the part element that contains HTML
-    cy.url().should('include', '/html') // new page URL should include '/html'
-  })
-  it('Takes us to nunjucks page', () => {
-    cy.visit('/')
-    cy.contains('NUNJUCK').click()
-    cy.url().should('include', '/nunjuck')
-  })
-  it('Takes us to spacecats page', () => {
-    cy.visit('/')
-    cy.contains('SPACECATS').click()
-    cy.url().should('include', '/spacecats')
-  })
+  
 })
+
