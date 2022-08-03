7cb0f4dd75040cd6cbac60b6e12e64a254e6d3b0
Korinna Pinakoulakis <kpinakoulaki@gmail.com>
2022-08-03 15:52:38 +0100

    add postcode page tests and add what is your name button navigation test

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,28 +1,4 @@
-describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
-
-// describe('Index Page Test', () => {
-//   it('Takes us to html page', () => {
-//     cy.visit('/')
-//     cy.contains('HTML').click() // click on the part element that contains HTML
-//     cy.url().should('include', '/html') // new page URL should include '/html'
-//   })
-//   it('Takes us to nunjucks page', () => {
-//     cy.visit('/')
-//     cy.contains('NUNJUCK').click()
-//     cy.url().should('include', '/nunjuck')
-//   })
-//   it('Takes us to spacecats page', () => {
-//     cy.visit('/')
-//     cy.contains('SPACECATS').click()
-//     cy.url().should('include', '/spacecats')
-//   })
-// })
-
-describe('Start Page Test', () => {
+describe('Start Page', () => {
   it("Loads successfully", () => {
     cy.visit('/') // fails if not running server
   })
@@ -46,7 +22,7 @@ describe('Start Page Test', () => {
   })
 })
 
-describe('"What is your name?" Page Test', () => {
+describe('"What is your name?" Page', () => {
   it("Loads successfully", () => {
     cy.visit('/what-is-your-name') // fails if not running server
   })
@@ -57,5 +33,26 @@ describe('"What is your name?" Page Test', () => {
   it("Has heading with 'What is your name?'", () => {
     cy.visit('/what-is-your-name')
     cy.get('[data-cy="page-heading"]').should('contain', 'What is your name?')
+  })
+  it("Continue button links to question page 'What is your postcode?'", () => {
+    cy.visit('/what-is-your-name')
+    cy.get('#first-name').type('Jane')
+    cy.get('#surname').type('Doe')
+    cy.get('[data-cy="continue-button"]').click()
+    cy.url().should('include', '/what-is-your-postcode')
+  })
+})
+
+describe('"What is your postcode?" Page', () => {
+  it("Loads successfully", () => {
+    cy.visit('/what-is-your-postcode')
+  })
+  it("Has title with 'confirm your identity'", () => {
+    cy.visit('/what-is-your-postcode')
+    cy.get('title').should('contain', 'Confirm your identity')
+  })
+  it("Has heading with 'What is your postcode?'", () => {
+    cy.visit('/what-is-your-postcode')
+    cy.get('[data-cy="page-heading"]').should('contain', 'What is your postcode?')
   })
 })
