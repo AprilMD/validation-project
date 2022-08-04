dddd7ae294215ff58ab9472cffa43d812d18bef2
Korinna Pinakoulakis <kpinakoulaki@gmail.com>
2022-08-04 12:22:27 +0100

    add tests for required fields

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -34,12 +34,23 @@ describe('"What is your name?" Page', () => {
     cy.visit('/what-is-your-name')
     cy.get('[data-cy="page-heading"]').should('contain', 'What is your name?')
   })
-  it("Continue button links to question page 'What is your postcode?'", () => {
+  it("Continue button links to question page 'What is your postcode?' when required fields are filled", () => {
     cy.visit('/what-is-your-name')
-    cy.get('#first-name').type('Jane')
-    cy.get('#surname').type('Doe')
+    cy.get('[data-cy="first-name"]').type('Kitty')
+    cy.get('[data-cy="surname"]').type('Moon')
     cy.get('[data-cy="continue-button"]').click()
     cy.url().should('include', '/what-is-your-postcode')
+  })
+  it("Continue button links to same page when required fields are not filled", () => {
+    cy.visit('/what-is-your-name')
+    cy.get('[data-cy="continue-button"]').click()
+    cy.url().should('include', '/what-is-your-name')
+  })
+  it("Continue button shows same page with error messages when required fields are not filled", () => {
+    cy.visit('/what-is-your-name')
+    cy.get('[data-cy="continue-button"]').click()
+    cy.get('[data-cy="first-name-error-message"]')
+    cy.get('[data-cy="surname-error-message"]')
   })
 })
 
